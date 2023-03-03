f61e7821811670f291b9fbadc6bffe1c26973973
Vitor Hugo Ayres <vitorayres1997@hotmail.com>
2023-03-02 21:41:35 -0300

    update swiper

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,9 +2,9 @@
 
 const blob = document.getElementById("blob");
 
-window.onpointermove = event => { 
+window.onpointermove = event => {
   const { clientX, clientY } = event;
-  
+
   blob.animate({
     left: `${clientX}px`,
     top: `${clientY}px`
@@ -17,27 +17,66 @@ const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 let interval = null;
 
-document.querySelector("h2").onmouseover = event => {  
+document.querySelector("h2").onmouseover = event => {
   let iteration = 0;
-  
+
   clearInterval(interval);
-  
+
   interval = setInterval(() => {
     event.target.innerText = event.target.innerText
       .split("")
       .map((letter, index) => {
-        if(index < iteration) {
+        if (index < iteration) {
           return event.target.dataset.value[index];
         }
-      
+
         return letters[Math.floor(Math.random() * 26)]
       })
       .join("");
-    
-    if(iteration >= event.target.dataset.value.length){ 
+
+    if (iteration >= event.target.dataset.value.length) {
       clearInterval(interval);
     }
-    
+
     iteration += 1 / 3;
   }, 30);
+}
+
+
+/* swiper effects */
+
+let activeIndex = 0;
+
+const groups = document.getElementsByClassName("card-group");
+
+const handleLoveClick = () => {
+  const nextIndex = activeIndex + 1 <= groups.length - 1 ? activeIndex + 1 : 0;
+
+  const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+    nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+
+  currentGroup.dataset.status = "after";
+
+  nextGroup.dataset.status = "becoming-active-from-before";
+
+  setTimeout(() => {
+    nextGroup.dataset.status = "active";
+    activeIndex = nextIndex;
+  });
+}
+
+const handleHateClick = () => {
+  const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : groups.length - 1;
+
+  const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+    nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+
+  currentGroup.dataset.status = "before";
+
+  nextGroup.dataset.status = "becoming-active-from-after";
+
+  setTimeout(() => {
+    nextGroup.dataset.status = "active";
+    activeIndex = nextIndex;
+  });
 }
